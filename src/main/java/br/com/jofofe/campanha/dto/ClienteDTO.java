2f01b3d12b6f8412b1cf1ad2d4e80b7420bcf2e5
João Felippe <jofelippe@hotmail.com>
2020-12-09 18:10:03 -0300

    Validações cliente bean validation

--- src/main/java/br/com/jofofe/campanha/dto/ClienteDTO.java
+++ src/main/java/br/com/jofofe/campanha/dto/ClienteDTO.java
@@ -3,8 +3,10 @@ package br.com.jofofe.campanha.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
-import java.util.List;
 
 @Getter
 @Setter
@@ -17,12 +19,16 @@ public class ClienteDTO extends BaseDTO {
 
     private Integer id;
 
+    @Email(message = "{email.valid}")
     private String email;
 
+    @NotBlank(message = "{nomeCompleto.not.blank}")
     private String nomeCompleto;
 
+    @NotNull(message = "{dataNascimento.not.null}")
     private Date dataNascimento;
 
+    @NotNull(message = "{time.not.null}")
     private TimeDTO time;
 
 }
