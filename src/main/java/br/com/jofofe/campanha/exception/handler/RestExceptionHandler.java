5fad1e7d825ebbd8d66796b161a5cde30cfb8730
João Felippe <jofelippe@hotmail.com>
2020-12-09 12:35:50 -0300

    Retorno das validações do Bean validation.

--- src/main/java/br/com/jofofe/campanha/exception/handler/RestExceptionHandler.java
+++ src/main/java/br/com/jofofe/campanha/exception/handler/RestExceptionHandler.java
@@ -2,12 +2,17 @@ package br.com.jofofe.campanha.exception.handler;
 
 import br.com.jofofe.campanha.exception.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -41,6 +46,19 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity timeNaoEncontrado(TimeNaoEncontradoException ex, WebRequest request) {
         log.debug("manipulação de TimeNaoEncontradoException...");
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.debug("manipulação de MethodArgumentNotValidException...");
+        List<String> errors = getErrors(ex);
+        return new ResponseEntity<>(errors, status);
+    }
+
+    private List<String> getErrors(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
     }
 
 }
